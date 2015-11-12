b3e70aa47ca6059c513cafbd233e42bb988a5898
Mark Anderson <mark@chef.io>
2015-11-11 19:16:35 -0800

    DNM test

--- config/software/oc-pushy-pedant.rb
+++ config/software/oc-pushy-pedant.rb
@@ -15,7 +15,7 @@
 #
 
 name "oc-pushy-pedant"
-default_version "master"
+default_version "ma/better_logging"
 
 # TODO: use the public git:// uri once this repo is public
 source git: "git@github.com:opscode/oc-pushy-pedant"
